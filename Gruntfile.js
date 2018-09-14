bf2147fbe3d9e0eb2b841f9abe7b078d1dc498dd
Leif Arne Genzmer <l.genzmer@neusta.de>
2018-09-14 10:38:37 +0200

    added gruntfile, packages and gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.initConfig({
+        sass:        {
+            dist: {
+                files: {
+                    'css/styles.css' : 'css/scss/styles.scss'
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'css/styles.min.css': ['css/styles.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/scss/*/*.scss',
+                tasks: ['sass', 'cssmin']
+            }
+        }
+    });
+    grunt.registerTask('build', ['sass', 'cssmin']);
+    grunt.registerTask('dev', ['sass', 'cssmin', 'watch']);
+};
